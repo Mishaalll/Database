4230e4230f98e0aadc9df656a339e3f4d67c7f31
Mishaalll <mikhail.ushakov.pp@gmail.com>
2025-10-31 11:16:42 +0200

    Add string comparison and copy functions

---
+++ mylib.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mylib.h"
+
+int mystrcmp(const char* s1, const char* s2)
+{
+    while (*s1 && *s2)
+    {
+        if (*s1 != *s2)
+            return *s1 - *s2;
+        s1++;
+        s2++;
+    }
+    return *s1 - *s2;
+}
+
+void mystrcpy(char* dest, const char* src)
+{
+    while (*src)
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+}
