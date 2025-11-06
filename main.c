254141f2b370119759b7f285e824d5c4782e371e
Mishaalll <mikhail.ushakov.pp@gmail.com>
2025-11-06 18:29:10 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 #include "mylib.h"
 
-struct birthdate 
-{
+struct birthdate {
     int day;
     int month;
     int year;
 };
 
-struct myData 
-{
+struct myData {
     int age;
     char *name;
     int year;
@@ -19,17 +17,15 @@ struct myData
 };
 
 #define MAX_STUDENTS 100
-struct myData database[MAX_STUDENTS];
-int total_students = 0;
 
 int main(int argc, char *argv[]) 
 {
+    struct myData database[MAX_STUDENTS];
+    int total_students = 0;
+
     FILE *fp = fopen("database.txt", "r");
-    if (fp) 
-    {
-        total_students = 0;
-        while (total_students < MAX_STUDENTS) 
-        {
+    if (fp) {
+        while (total_students < MAX_STUDENTS) {
             database[total_students].name = malloc(50);
             int res = fscanf(fp, "%d %49s %d %9s %d %d %d",
                 &database[total_students].age,
@@ -39,7 +35,10 @@ int main(int argc, char *argv[])
                 &database[total_students].bdate.day,
                 &database[total_students].bdate.month,
                 &database[total_students].bdate.year);
-            if (res != 7) break;
+            if (res != 7) {
+                free(database[total_students].name);
+                break;
+            }
             total_students++;
         }
         fclose(fp);
@@ -49,84 +48,73 @@ int main(int argc, char *argv[])
     {
         if (mystrcmp(argv[1], "add") == 0 && argc == 9) 
         {
-            if (total_students >= MAX_STUDENTS) 
-            {
-                printf("Database full.\n");
-                goto save;
-            }
+            if (total_students < MAX_STUDENTS) {
+                int age = atoi(argv[2]);
+                char *name = argv[3];
+                int year = atoi(argv[4]);
+                char *course = argv[5];
+                int day = atoi(argv[6]);
+                int month = atoi(argv[7]);
+                int byear = atoi(argv[8]);
 
-            int age = atoi(argv[2]);
-            char *name = argv[3];
-            int year = atoi(argv[4]);
-            char *course = argv[5];
-            int day = atoi(argv[6]);
-            int month = atoi(argv[7]);
-            int byear = atoi(argv[8]);
-
-            database[total_students].age = age;
-            database[total_students].name = malloc(50);
-            mystrcpy(database[total_students].name, name);
-            mystrcpy(database[total_students].course, course);
-            database[total_students].year = year;
-            database[total_students].bdate.day = day;
-            database[total_students].bdate.month = month;
-            database[total_students].bdate.year = byear;
-            total_students++;
-        } 
+                database[total_students].age = age;
+                database[total_students].name = malloc(50);
+                mystrcpy(database[total_students].name, name);
+                mystrcpy(database[total_students].course, course);
+                database[total_students].year = year;
+                database[total_students].bdate.day = day;
+                database[total_students].bdate.month = month;
+                database[total_students].bdate.year = byear;
+                total_students++;
+                printf("Student added.\n");
+            } else printf("Database full.\n");
+        }
         else if (mystrcmp(argv[1], "del") == 0 && argc == 3) 
         {
             char *name = argv[2];
-            for (int i = 0; i < total_students; i++) 
-            {
-                if (mystrcmp(database[i].name, name) == 0) 
-                {
+            int found = 0;
+            for (int i = 0; i < total_students; i++) {
+                if (mystrcmp(database[i].name, name) == 0) {
                     free(database[i].name);
                     for (int j = i; j < total_students - 1; j++)
                         database[j] = database[j + 1];
                     total_students--;
+                    found = 1;
+                    printf("Deleted %s.\n", name);
                     break;
                 }
             }
-        }
-        else if (mystrcmp(argv[1], "help") == 0)
-        {
-            printf("Available commands:\n");
-            printf("  help                     - show this help message\n");
-            printf("  add [AGE NAME YEAR COURSE DAY MONTH BYEAR] - add a new student\n");
-            printf("  del [NAME]               - delete a student by name\n");
-            printf("  sort_age                 - sort students by age\n");
+            if (!found) printf("Not found.\n");
         }
         else if (mystrcmp(argv[1], "sort_age") == 0) 
         {
-            for (int i = 0; i < total_students; i++) 
-            {
-                for (int j = i + 1; j < total_students; j++) 
-                {
-                    if (database[i].age > database[j].age) 
-                    {
-                        struct myData temp = database[i];
+            for (int i = 0; i < total_students; i++)
+                for (int j = i + 1; j < total_students; j++)
+                    if (database[i].age > database[j].age) {
+                        struct myData tmp = database[i];
                         database[i] = database[j];
-                        database[j] = temp;
+                        database[j] = tmp;
                     }
-                }
-            }
-        } 
-        else 
+            printf("Sorted by age.\n");
+        }
+        else if (mystrcmp(argv[1], "help") == 0)
         {
-            printf("Unknown command. Use 'help'.\n");
+            printf("Commands:\n");
+            printf("add [AGE NAME YEAR COURSE DAY MONTH BYEAR]\n");
+            printf("del [NAME]\n");
+            printf("sort_age\n");
+            printf("help\n");
         }
     }
-
-save:
     fp = fopen("database.txt", "w");
-    if (!fp) 
-    {
-        printf("Cannot open file for writing.\n");
+    if (!fp) {
+        printf("File write error.\n");
+        for (int i = 0; i < total_students; i++)
+            free(database[i].name);
         return 1;
     }
 
-    for (int i = 0; i < total_students; i++) 
-    {
+    for (int i = 0; i < total_students; i++)
         fprintf(fp, "%d %s %d %s %d %d %d\n",
             database[i].age,
             database[i].name,
@@ -135,7 +123,7 @@ save:
             database[i].bdate.day,
             database[i].bdate.month,
             database[i].bdate.year);
-    }
+
     fclose(fp);
 
     for (int i = 0; i < total_students; i++)
