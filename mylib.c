8adc6be377e28d3c15fbfc2221c27bb050316cbb
Mishaalll <mikhail.ushakov.pp@gmail.com>
2025-11-14 01:28:08 +0200

    Implement myatoi and mystrlen functions

--- mylib.c
+++ mylib.c
@@ -24,3 +24,44 @@ void mystrcpy(char* dest, const char* src)
     }
     *dest = '\0';
 }
+
+
+
+int myatoi(const char *str) 
+{
+    int res = 0;
+    int sign = 1;
+    int i = 0;
+
+    if (str[0] == '-') 
+    {
+        sign = -1;
+        i++;
+    }
+
+    for (; str[i] != '\0'; i++) 
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            res = res * 10 + (str[i] - '0');
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return res * sign;
+}
+
+
+
+int mystrlen(const char *str) 
+{
+    int len = 0;
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+    return len;
+}
