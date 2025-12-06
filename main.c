f1e2a90f2ba7ebe3d4385995208ad95a90733672
Mishaalll <mikhail.ushakov.pp@gmail.com>
2025-12-06 16:34:31 +0200

    fix

--- main.c
+++ main.c
@@ -24,13 +24,10 @@ int main(int argc, char *argv[])
     int total_students = 0;
 
     printf("Commands:\n");
-    printf("add [AGE NAME YEAR COURSE DAY MONTH BYEAR]\n");
-    printf("del [NAME]\n");
-    printf("sort_age\n");
-    printf("sort_name\n");
-    printf("sort_birthyear\n");
-    printf("sort_course\n");
-    printf("help\n");
+    printf("-add [-age -name -year -course -day -month -byear]\n");
+    printf("-del [NAME]\n");
+    printf("-sort [-age -name -year -course -day -month -byear]\n");
+    printf("-help\n");
 
 
     FILE *fp = fopen("database.txt", "r");
@@ -80,38 +77,77 @@ int main(int argc, char *argv[])
 
     if (argc >= 2) 
     {
-        if (mystrcmp(argv[1], "add") == 0 && argc == 9) 
+        if (mystrcmp(argv[1], "-add") == 0)
         {
-            struct myData *new_db = malloc((total_students + 1) * sizeof(struct myData));
-            for (int i = 0; i < total_students; i++) new_db[i] = database[i];
-            free(database);
-            database = new_db;
+            int age = -1;
+            char *name = NULL;
+            int year = -1;
+            char course[10] = {0};
+            int day = -1, month = -1, byear = -1;
 
-            database[total_students].age = myatoi(argv[2]);
-            database[total_students].name = malloc(mystrlen(argv[3]) + 1);
-            mystrcpy(database[total_students].name, argv[3]);
-            database[total_students].year = myatoi(argv[4]);
-            mystrcpy(database[total_students].course, argv[5]);
-            database[total_students].bdate.day = myatoi(argv[6]);
-            database[total_students].bdate.month = myatoi(argv[7]);
-            database[total_students].bdate.year = myatoi(argv[8]);
-            total_students++;
-
-            printf("Student added:\n");
-            for (int i = 0; i < total_students; i++)
+            for (int i = 2; i < argc; i++)
             {
-                printf("%d %s %d %s %d %d %d\n",
-                    database[i].age,
-                    database[i].name,
-                    database[i].year,
-                    database[i].course,
-                    database[i].bdate.day,
-                    database[i].bdate.month,
-                    database[i].bdate.year);
+                if (mystrcmp(argv[i], "-age") == 0 && i + 1 < argc)
+                    age = myatoi(argv[++i]);
+
+                else if (mystrcmp(argv[i], "-name") == 0 && i + 1 < argc)
+                    name = argv[++i];
+
+                else if (mystrcmp(argv[i], "-year") == 0 && i + 1 < argc)
+                    year = myatoi(argv[++i]);
+
+                else if (mystrcmp(argv[i], "-course") == 0 && i + 1 < argc)
+                    mystrcpy(course, argv[++i]);
+
+                else if (mystrcmp(argv[i], "-day") == 0 && i + 1 < argc)
+                    day = myatoi(argv[++i]);
+
+                else if (mystrcmp(argv[i], "-month") == 0 && i + 1 < argc)
+                    month = myatoi(argv[++i]);
+
+                else if (mystrcmp(argv[i], "-byear") == 0 && i + 1 < argc)
+                    byear = myatoi(argv[++i]);
             }
-            printf("\n");
+
+            if (age == -1 || name == NULL || year == -1 ||
+                course[0] == 0 || day == -1 || month == -1 || byear == -1)
+            {
+                printf("Required: -name -age -year -course -day -month -byear\n");
+            }
+            else
+            {
+                struct myData *new_db = malloc((total_students + 1) * sizeof(struct myData));
+                for (int i = 0; i < total_students; i++) new_db[i] = database[i];
+                free(database);
+                database = new_db;
+
+                database[total_students].age = age;
+                database[total_students].name = malloc(mystrlen(name) + 1);
+                mystrcpy(database[total_students].name, name);
+                database[total_students].year = year;
+                mystrcpy(database[total_students].course, course);
+                database[total_students].bdate.day = day;
+                database[total_students].bdate.month = month;
+                database[total_students].bdate.year = byear;
+
+                total_students++;
+
+                printf("Student added:\n");
+                for (int i = 0; i < total_students; i++)
+                {
+                    printf("%d %s %d %s %d %d %d\n",
+                        database[i].age,
+                        database[i].name,
+                        database[i].year,
+                        database[i].course,
+                        database[i].bdate.day,
+                        database[i].bdate.month,
+                        database[i].bdate.year);
+                }
+                printf("\n");
+            }
         }
-        else if (mystrcmp(argv[1], "del") == 0 && argc == 3) 
+        else if (mystrcmp(argv[1], "-del") == 0 && argc == 3) 
         {
             char *name = argv[2];
             int found = 0;
@@ -147,23 +183,93 @@ int main(int argc, char *argv[])
                 printf("Not found.\n");
             }
         }
-        else if (mystrcmp(argv[1], "sort_age") == 0) 
+        else if (mystrcmp(argv[1], "-sort") == 0 && argc == 3)
         {
-            for (int i = 0; i < total_students; i++)
+            if (mystrcmp(argv[2], "-age") == 0)
             {
-                for (int j = i + 1; j < total_students; j++)
-                {
-                    if (database[i].age > database[j].age) 
-                    {
-                        struct myData tmp = database[i];
-                        database[i] = database[j];
-                        database[j] = tmp;
-                    }
-                }
-            }    
-            printf("Sorted by age:\n");
-            for (int i = 0; i < total_students; i++)
+                for (int i = 0; i < total_students; i++)
+                    for (int j = i + 1; j < total_students; j++)
+                        if (database[i].age > database[j].age)
+                        {
+                            struct myData t = database[i];
+                            database[i] = database[j];
+                            database[j] = t;
+                        }
+            }
+
+            else if (mystrcmp(argv[2], "-name") == 0)
             {
+                for (int i = 0; i < total_students; i++)
+                    for (int j = i + 1; j < total_students; j++)
+                        if (mystrcmp(database[i].name, database[j].name) > 0)
+                        {
+                            struct myData t = database[i];
+                            database[i] = database[j];
+                            database[j] = t;
+                        }
+            }
+
+            else if (mystrcmp(argv[2], "-course") == 0)
+            {
+                for (int i = 0; i < total_students; i++)
+                    for (int j = i + 1; j < total_students; j++)
+                        if (mystrcmp(database[i].course, database[j].course) > 0)
+                        {
+                            struct myData t = database[i];
+                            database[i] = database[j];
+                            database[j] = t;
+                        }
+            }
+
+            else if (mystrcmp(argv[2], "-birthyear") == 0)
+            {
+                for (int i = 0; i < total_students; i++)
+                    for (int j = i + 1; j < total_students; j++)
+                        if (database[i].bdate.year > database[j].bdate.year)
+                        {
+                            struct myData t = database[i];
+                            database[i] = database[j];
+                            database[j] = t;
+                        }
+            }
+
+            else if (mystrcmp(argv[2], "-year") == 0)
+            {
+                for (int i = 0; i < total_students; i++)
+                    for (int j = i + 1; j < total_students; j++)
+                        if (database[i].year > database[j].year)
+                        {
+                            struct myData t = database[i];
+                            database[i] = database[j];
+                            database[j] = t;
+                        }
+            }
+
+            else if (mystrcmp(argv[2], "-day") == 0)
+            {
+                for (int i = 0; i < total_students; i++)
+                    for (int j = i + 1; j < total_students; j++)
+                        if (database[i].bdate.day > database[j].bdate.day)
+                        {
+                            struct myData t = database[i];
+                            database[i] = database[j];
+                            database[j] = t;
+                        }
+            }
+
+            else if (mystrcmp(argv[2], "-month") == 0)
+            {
+                for (int i = 0; i < total_students; i++)
+                    for (int j = i + 1; j < total_students; j++)
+                        if (database[i].bdate.month > database[j].bdate.month)
+                        {
+                            struct myData t = database[i];
+                            database[i] = database[j];
+                            database[j] = t;
+                        }
+            }
+
+            for (int i = 0; i < total_students; i++)
                 printf("%d %s %d %s %d %d %d\n",
                     database[i].age,
                     database[i].name,
@@ -172,107 +278,17 @@ int main(int argc, char *argv[])
                     database[i].bdate.day,
                     database[i].bdate.month,
                     database[i].bdate.year);
-            }
+
             printf("\n");
         }
 
-        else if (mystrcmp(argv[1], "sort_name") == 0)
-        {
-            for (int i = 0; i < total_students; i++)
-            {    for (int j = i + 1; j < total_students; j++)
-                {
-                    if (mystrcmp(database[i].name, database[j].name) > 0)
-                    {
-                        struct myData tmp = database[i];
-                        database[i] = database[j];
-                        database[j] = tmp;
-            }   }   }
-            printf("Sorted by name:\n");
-            for (int i = 0; i < total_students; i++)
-            {
-                printf("%d %s %d %s %d %d %d\n",
-                    database[i].age,
-                    database[i].name,
-                    database[i].year,
-                    database[i].course,
-                    database[i].bdate.day,
-                    database[i].bdate.month,
-                    database[i].bdate.year);
-            }
-            printf("\n");
-
-        }
-
-        else if (mystrcmp(argv[1], "sort_birthyear") == 0)
-        {
-            for (int i = 0; i < total_students; i++)
-            {    
-                for (int j = i + 1; j < total_students; j++)
-                {
-                    if (database[i].bdate.year > database[j].bdate.year)
-                    {
-                        struct myData tmp = database[i];
-                        database[i] = database[j];
-                        database[j] = tmp;
-                    }
-                }
-            }
-            printf("Sorted by birthyear:\n");
-            for (int i = 0; i < total_students; i++)
-            {
-                printf("%d %s %d %s %d %d %d\n",
-                    database[i].age,
-                    database[i].name,
-                    database[i].year,
-                    database[i].course,
-                    database[i].bdate.day,
-                    database[i].bdate.month,
-                    database[i].bdate.year);
-            }
-            printf("\n");
-        }
-
-
-        else if (mystrcmp(argv[1], "sort_course") == 0)
-        {
-            for (int i = 0; i < total_students; i++)
-            {
-                for (int j = i + 1; j < total_students; j++)
-                {
-                    if (mystrcmp(database[i].course, database[j].course) > 0)
-                    {
-                        struct myData tmp = database[i];
-                        database[i] = database[j];
-                        database[j] = tmp;
-                    }
-                }
-            }
-            printf("Sorted by course:\n");
-            for (int i = 0; i < total_students; i++)
-            {
-                printf("%d %s %d %s %d %d %d\n",
-                    database[i].age,
-                    database[i].name,
-                    database[i].year,
-                    database[i].course,
-                    database[i].bdate.day,
-                    database[i].bdate.month,
-                    database[i].bdate.year);
-            }
-            printf("\n");
-        }
-
-
-        else if (mystrcmp(argv[1], "help") == 0)
+        else if (mystrcmp(argv[1], "-help") == 0)
         {
             printf("Commands:\n");
-            printf("add [AGE NAME YEAR COURSE DAY MONTH BYEAR]\n");
-            printf("del [NAME]\n");
-            printf("sort_age\n");
-            printf("sort_name\n");
-            printf("sort_birthyear\n");
-            printf("sort_course\n");
-            printf("help\n");
+            printf("-add [-age -name -year -course -day -month -byear]\n");
+            printf("-del [NAME]\n");
+            printf("-sort [-age -name -year -course -day -month -byear]\n");
+            printf("-help\n");
         }
     }
 
